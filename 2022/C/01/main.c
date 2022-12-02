f1b3920cb9bdfc1807f266c290535d6ad768be7e
Sinan Pasic <sinan.p.pasic@gmail.com>
2022-12-02 06:12:45 +0100

    Moved from naive native C solution to a more elegant solution written using third party header library bradenlib

--- 2022/C/01/main.c
+++ 2022/C/01/main.c
@@ -1,41 +1,46 @@
+// External header files used here are part of bradenlib, a header library for
+// general purpose C code written by Braden Best. Link to his GitLab and library here:
+// https://gitlab.com/bradenbest/bradenlib
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "/home/bolvarsdad/Code/C/bradenlib/headers/strntol.h"
+#include "/home/bolvarsdad/Code/C/bradenlib/headers/for_each_line.h"
+
+long totals[500];
+size_t totals_len = 0;
+
 int
-main(int argc, char **argv)
+qsort_fn(const void *a, const void *b)
 {
-    FILE *data = fopen("input.txt", "r");
+    return (*(long *)b - *(long *)a);
+}
 
-    int max[3] = {0};
-    int kcal = 0;
-    char line[10];
+// Function that's used as for_each_line_fn, read the well-written documentaton
+// inside of for_each_line.h and the test file tfor_each_line.c.
+void
+add_line(char const *line, size_t len)
+{
+    long value = strntol(line, len, 10);
 
-    while (fgets(line, 10, data))
-    {
-        if (line[0] == '\n' || line[0] == '\r')
-        {
-            if (kcal > max[0])
-            {
-                max[2] = max[1];
-                max[1] = max[0];
-                max[0] = kcal;
-            }
-            else if (kcal > max[1])
-            {
-                max[2] = max[1];
-                max[1] = kcal;
-            }
-            else if (kcal > max[2])
-                max[2] = kcal;
+    totals[totals_len] += value;
+}
 
-            kcal = 0;
-        }
-        else
-            kcal += atoi(line);
-    }
+int main(void)
+{
+    char buffer[4096];
+    FILE *file = fopen("input.txt", "r");
 
-    printf("%d\n", max[0]);
-    printf("%d\n", max[0] + max[1] + max[2]);
+    while (for_each_line(buffer, 4096, file, add_line))
+            ++totals_len;
+
+    ++totals_len;
+
+    qsort(totals, totals_len, sizeof(long), qsort_fn);
+
+    printf("Part one: %lu\n", totals[0]);
+    printf("Part two: %lu\n", totals[0] + totals[1] + totals[2]);
 
     return 0;
 }

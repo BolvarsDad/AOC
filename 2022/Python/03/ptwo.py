bded65739cc7437b5324808e8232b9876277e93d
Sinan Pasic <sinan.p.pasic@gmail.com>
2022-12-04 09:31:51 +0100

    updated part 2

--- 2022/Python/03/ptwo.py
+++ 2022/Python/03/ptwo.py
@@ -1,20 +1,18 @@
 import string
 
-def score(char):
-    return ord(char) - ord('a') + 1 if char.islower() else ord(char) - ord('A') + 27
-
 bsum = 0
+alphabet = string.ascii_lowercase + string.ascii_uppercase
+charset = set(alphabet)
 
 with open("input.txt") as file:
     rucksacks = file.read().splitlines()
 
     for i in range(0, len(rucksacks), 3):
-        elf1 = set(rucksacks[i])
-        elf2 = set(rucksacks[i + 1])
-        elf3 = set(rucksacks[i + 2])
+        elf1, elf2, elf3 = map(set, rucksacks[i:i+3])
 
-        match = elf1 & elf2 & elf3
+        priorities = [alphabet.index((elf1 & elf2 & elf3).pop()) + 1]
 
-        bsum += sum(map(score, match))
+        for thing in priorities:
+            bsum += thing
 
     print(bsum)

43f4d0f99afdcf416e20d74d7ef2c1d9846ab8ed
Sinan Pasic <sinan.p.pasic@gmail.com>
2022-12-04 09:40:57 +0100

    part 2

--- 2022/Python/03/ptwo.py
+++ 2022/Python/03/ptwo.py
@@ -2,7 +2,6 @@ import string
 
 bsum = 0
 alphabet = string.ascii_lowercase + string.ascii_uppercase
-charset = set(alphabet)
 
 with open("input.txt") as file:
     rucksacks = file.read().splitlines()
@@ -12,7 +11,6 @@ with open("input.txt") as file:
 
         priorities = [alphabet.index((elf1 & elf2 & elf3).pop()) + 1]
 
-        for typeval in priorities:
-            bsum += typeval
+        bsum += sum(priorities)
 
     print(bsum)

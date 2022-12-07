2c0b75412976826b6504fc2c92360cff5125ed16
Sinan Pasic <sinan.p.pasic@gmail.com>
2022-12-07 11:02:18 +0100

    Python day 6

---
+++ 2022/Python/06/main.py
@@ -0,0 +1,9 @@
+def find_distinct(message, n):
+    for i in range(len(message)-n-1):
+        if len(set(message[i:i+n])) == n:
+            return i+n
+    return None
+
+contents = open("input.txt").read()
+print("Part 1:", find_distinct(contents, 4))
+print("Part 2:", find_distinct(contents, 14))
